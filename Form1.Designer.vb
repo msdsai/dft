80d208df72be118c0253b35dc7b7da63f463a181
deekshitha <d.madala@iitg.ac.in>
2024-06-17 11:10:50 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Panel1 = New Panel()
+        Panel2 = New Panel()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Anchor = AnchorStyles.Left Or AnchorStyles.Right
+        Button1.Font = New Font("Segoe UI", 10F)
+        Button1.Location = New Point(729, 507)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(151, 50)
+        Button1.TabIndex = 0
+        Button1.Text = "Select vowel"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Panel1.AutoScroll = True
+        Panel1.Location = New Point(70, 104)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(684, 308)
+        Panel1.TabIndex = 2
+        ' 
+        ' Panel2
+        ' 
+        Panel2.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        Panel2.AutoScroll = True
+        Panel2.Location = New Point(868, 104)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(692, 308)
+        Panel2.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(1097, 448)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(133, 20)
+        Label1.TabIndex = 4
+        Label1.Text = "Frequency Domain"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(340, 448)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(95, 20)
+        Label2.TabIndex = 5
+        Label2.Text = "TimeDomain"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1637, 594)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,134 @@
+﻿Imports System.IO
+Imports System.Numerics
+
+Public Class Form1
+    Private integerList As New List(Of Double)()
+    Private dftMagnitudes As New List(Of Double)()
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Create an instance of the OpenFileDialog
+        Dim openFileDialog1 As New OpenFileDialog()
+
+        ' Set the filter for file types
+        openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+
+        ' Show the Open File dialog
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            ' Get the path of the selected file
+            Dim filePath As String = openFileDialog1.FileName
+
+            ' Clear the previous data
+            integerList.Clear()
+            dftMagnitudes.Clear()
+
+            ' Read the file line by line
+            Try
+                Using sr As New StreamReader(filePath)
+                    Dim line As String
+                    line = sr.ReadLine()
+                    While line IsNot Nothing
+                        Dim number As Double
+                        If Double.TryParse(line, number) Then
+                            integerList.Add(number)
+                        End If
+                        line = sr.ReadLine()
+                    End While
+                End Using
+
+                Panel1.Invalidate()
+                ' Compute the DFT and magnitudes
+                ComputeDFT()
+
+                ' Refresh the Panel to trigger the Paint event
+                Panel2.Invalidate()
+
+            Catch ex As Exception
+                MessageBox.Show("Error reading file: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub ComputeDFT()
+        Dim N As Integer = integerList.Count
+        Dim dftResult(N - 1) As Complex
+
+        For k As Integer = 0 To N - 1
+            Dim sum As Complex = Complex.Zero
+            For innerN As Integer = 0 To N - 1
+                Dim angle As Double = -2.0 * Math.PI * k * innerN / N
+                sum += integerList(innerN) * New Complex(Math.Cos(angle), Math.Sin(angle))
+            Next
+            dftResult(k) = sum
+            dftMagnitudes.Add(dftResult(k).Magnitude)
+        Next
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+        Dim g As Graphics = e.Graphics
+        Dim panelWidth As Integer = Panel1.ClientSize.Width
+        Dim panelHeight As Integer = Panel1.ClientSize.Height - 1
+
+        If integerList.Count > 0 Then
+            ' Determine the scale factors
+            Dim maxValue As Double = integerList.Max()
+            Dim xScale As Single = panelWidth / integerList.Count
+            Dim yScale As Single = If(maxValue <> 0, panelHeight / maxValue, 1)
+
+            ' Draw the axes
+            g.DrawLine(Pens.Black, 0, panelHeight, panelWidth, panelHeight)
+            g.DrawLine(Pens.Black, 0, 0, 0, panelHeight)
+
+            ' Draw the graph
+            For i As Integer = 0 To integerList.Count - 1
+                Dim x As Single = i * xScale
+                Dim y As Single = panelHeight - (integerList(i) * yScale)
+                g.FillEllipse(Brushes.Blue, x, y, 0, 0)
+
+                If i > 0 Then
+                    Dim prevX As Single = (i - 1) * xScale
+                    Dim prevY As Single = panelHeight - (integerList(i - 1) * yScale)
+                    g.DrawLine(Pens.Blue, prevX, prevY, x, y)
+                End If
+            Next
+        End If
+    End Sub
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+        Dim g = e.Graphics
+        Dim panelWidth = Panel2.ClientSize.Width - 10
+        Dim panelHeight = Panel2.ClientSize.Height - 10
+
+        If dftMagnitudes.Count > 0 Then
+            ' Determine the scale factors
+            Dim maxValue = dftMagnitudes.Max
+            Dim xScale As Single = (panelWidth) / dftMagnitudes.Count
+            Dim yScale As Single = If(maxValue <> 0, panelHeight / maxValue, 1)
+
+            ' Draw the axes
+            g.DrawLine(Pens.Black, 0, panelHeight, panelWidth, panelHeight)
+            g.DrawLine(Pens.Black, 0, 0, 0, panelHeight)
+            'g.DrawLine(Pens.Black, 0, 0, panelWidth, 0)
+            'g.DrawLine(Pens.Black, panelWidth, 0, panelWidth, panelHeight)
+
+            ' Draw the graph
+            For i = 0 To dftMagnitudes.Count - 1
+                Dim x = i * xScale
+                Dim y As Single = panelHeight - dftMagnitudes(i) * yScale
+                g.FillEllipse(Brushes.Blue, x, y, 0, 0)
+
+                If i > 0 Then
+                    Dim prevX = (i - 1) * xScale
+                    Dim prevY As Single = panelHeight - dftMagnitudes(i - 1) * yScale
+                    g.DrawLine(Pens.Blue, prevX, prevY, x, y)
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+End Class
